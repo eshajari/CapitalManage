42bb5953aa307a022b3ba487a5220ee0ce945b97
Harsh Pancholi <esharsh@esharshs-macbook-air.local>
2016-06-06 12:09:32 +0530

    Initial Commit

---
+++ CapitalManage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CapitalManage
+//
+//  Created by esharsh on 06/06/16.
+//  Copyright © 2016 Esha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CapitalManage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CapitalManage
+//
+//  Created by esharsh on 06/06/16.
+//  Copyright © 2016 Esha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
